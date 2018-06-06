e8d4f7907d92b2e3b5dd9d1c90845a30ec93ea6b
Inna19 <DanilovaInna1987@mail.ru>
2018-06-06 09:10:25 +0300

    example of rk for problem

---
+++ initial_value_problem_and_rk/rungekuttaforivptest.py
@@ -0,0 +1,19 @@
+from nodepy import rk
+import numpy as np
+from matplotlib import mlab
+import matplotlib.pyplot as plt
+import pylab
+import ivptest
+from ivptest import detest
+rk44 = rk.loadRKM('RK44')#load runge-kutta
+ivp=ivptest
+myivp=ivptest.detest('2OD1')#load initial parameters from problem
+t,u = rk44(myivp)#approximate soluton of problem
+
+plt.axis([0, 5,0,120])
+plt.plot(t, u,)#model solution
+plt.xlabel('t')
+plt.ylabel('u')
+plt.legend(['x(t)', 'y(t)'], loc = 'central')
+plt.grid()
+plt.show() 
