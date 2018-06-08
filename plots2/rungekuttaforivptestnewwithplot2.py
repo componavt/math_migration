dd87192fe1bc8a55afb447a6ea56d0b54b0c2782
Inna19 <DanilovaInna1987@mail.ru>
2018-06-08 17:28:07 +0300

    newplotsadd

---
+++ plots2/rungekuttaforivptestnewwithplot2.py
@@ -0,0 +1,29 @@
+from nodepy import rk
+import numpy as np
+from matplotlib import mlab
+import matplotlib.pyplot as plt
+import pylab
+import ivptest
+from ivptest import detest
+rk44 = rk.loadRKM('RK44')#load runge-kutta
+ivp=ivptest
+myivp=ivptest.detest('2OD1')#load initial parameters from problem
+t,u = rk44(myivp)#approximate soluton of problem
+
+plt.axis([0, 10,0,20])
+plt.plot(t, u,)#model solution
+plt.xlabel('t')
+plt.ylabel('u')
+plt.legend(['x(t)', 'y(t)'], loc = 'central')
+plt.grid()
+#plt.savefig(str(i)+'.png', format='png', dpi=100)
+plt.show()
+for i in range(1):
+    plt.axis([0, 10,0,20])
+    plt.plot(t, u,)
+    plt.xlabel('t')
+    plt.ylabel('u')
+    plt.legend(['x(t)', 'y(t)'], loc = 'central')
+    plt.grid()
+    plt.savefig(str(i)+'.png', format='png')
+#plt.savefig(str(i)+'.png', format='png')
