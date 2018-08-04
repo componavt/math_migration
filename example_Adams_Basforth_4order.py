350a30faffd38b0894680524c1377d065815bc83
Inna19 <DanilovaInna1987@mail.ru>
2018-08-04 17:51:15 +0300

    add

--- example_Adams_Basforth_4order.py
+++
@@ -1,7 +0,0 @@
-from nodepy import ivp
-import nodepy.linear_multistep_method as lm
-myivp = ivp.detest('B1')#loading the initial data of task B1, which is described in ivp.py
-print(myivp)
-ab4=lm.Adams_Bashforth(4)#call method Adams-Bashforth
-t, y = ab4(myivp)# integrate of B1 by method Adams-Bashforth
-print(t,y)
--- example_Runge-Kutta.py
+++
@@ -1,6 +0,0 @@
-from nodepy import ivp,rk
-myivp = ivp.detest('B1')#loading the initial data of task B1, which is described in ivp.py
-print(myivp)
-rk4 = rk.loadRKM('RK44')#call method Runge - Kutta
-t,y = rk4(myivp)#integrate of B1 by method Runge-Kutta
-print(t,y)
